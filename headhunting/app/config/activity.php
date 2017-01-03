5abf88e3d115ab7192465c363581bf104ba47ebc
ankurbhati03 <ankurbhati03@gmail.com>
2017-01-03 22:58:45 +0530

    a

---
+++ headhunting/app/config/activity.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'third_party_multi_upload' => '%s has uploaded %d new third parties and %d existing ones', //type 2 
+	'third_party_single_upload'=>'%s has added %s third party', //type 3
+	'client_multi_upload' => '%s has uploaded %d clients', //type 4
+	'client_single_upload'=>'%s has added %s client', //type 5
+	'job_post_creation'=>'%s has created %s requirement',  //type 6
+	'job_post_assign'=>'%s has assigned %s requirement to %s',  //type 7
+	'job_post_submission'=>'%s has submitted %s requirement', //type 9
+	'candidate_add' =>'%s has added %s candidate' //type 10
+);
