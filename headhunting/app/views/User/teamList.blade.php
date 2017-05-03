e282ae2c323b7b0a0714bfbbd54bf59bd3f6d5b0
root <root@CRMserver.apetan.com>
2017-05-04 01:15:01 +0530

    changes into views.

--- headhunting/app/views/User/teamList.blade.php
+++ headhunting/app/views/User/teamList.blade.php
@@ -35,15 +35,15 @@
                             <td>{{$managerUser->userRoles[0]->roles->role}}</td>
                             <td>
                             @if($jobPostId > 0 && $jobPost->jobsAssignedToId($managerUser->id)->count() == 0)
-                              <a href="{{ URL::route('assign-requirement', array('id' => $jobPostId, 'assignedTo' => $managerUser->id )) }}" title="Assign To {{$managerUser->first_name}}"><i class="fa fa-plus"></i>Assign Job Post</a>
+                              <a href="{{ URL::route('assign-requirement', array('id' => $jobPostId, 'assignedTo' => $managerUser->id )) }}" title="Assign To {{$managerUser->first_name}}" class="btn btn-primary btn-white" ></i>Assign Job Post</a>
                             @else
                               @if($jobPostId > 0)
                                 Already Assigned
                               @endif
                             @endif
-                            <a href="{{ URL::route('my-activity', array('id' => $managerUser->id)) }}" title="View Activity"><i class="fa fa-fw fa-eye"></i></a>
+                            <a href="{{ URL::route('my-activity', array('id' => $managerUser->id)) }}" class="btn btn-primary btn-white" title="View Activity">View Activity</a>
                             @if(Auth::user()->hasRole(1))
-                            <a href="{{ URL::route('user-report', array('id' => $managerUser->id)) }}" title="View Reports"><i class="fa fa-fw fa-eye"></i></a>
+                            <a href="{{ URL::route('user-report', array('id' => $managerUser->id)) }}" class="btn btn-primary btn-white"  title="View Reports">View Report</a>
                             @endif
                             </td>
                           </tr> 
@@ -56,13 +56,13 @@
 		                        <td>{{$user->user->userRoles[0]->roles->role}}</td>
                             <td>
                             @if($jobPostId > 0 && $jobPost->jobsAssignedToId($user->user->id)->count() == 0)
-                              <a href="{{ URL::route('assign-requirement', array('id' => $jobPostId, 'assignedTo' => $user->user->id )) }}" title="Assign To {{$user->user->first_name}}"><i class="fa fa-plus"></i>Assign Job Post</a>
+                              <a href="{{ URL::route('assign-requirement', array('id' => $jobPostId, 'assignedTo' => $user->user->id )) }}" class="btn btn-primary btn-white" title="Assign To {{$user->user->first_name}}">Assign To {{$user->user->first_name}}</a>
                             @else
                               @if($jobPostId > 0)
                                 Already Assigned
                               @endif
                             @endif
-                            <a href="{{ URL::route('my-activity', array('id' => $user->user->id)) }}" title="View Activity"><i class="fa fa-fw fa-eye"></i></a>
+                            <a href="{{ URL::route('my-activity', array('id' => $user->user->id)) }}"  class="btn btn-primary btn-white" title="View Activity">View Activity</a>
                             </td>
 		                      </tr> 
 						          @endforeach
